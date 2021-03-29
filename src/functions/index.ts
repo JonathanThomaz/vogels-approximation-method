04bbad8ab9f55d160070e4b0a1456645997afa82
Jonathan Thomaz <jonathanthomaz96@gmail.com>
2021-03-29 12:46:39 -0300

    fix errors

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -20,7 +20,11 @@ export function calcDifference(
         array.splice(array.indexOf(min), 1);
         min2 = Math.min(...array);
         difference = min2 - min;
-        differences.push(difference);
+        if (min === 99 || min2 === 99) {
+            differences.push(-1);
+        } else {
+            differences.push(difference);
+        }
     }
     return differences;
 }
@@ -38,7 +42,7 @@ export function getResultado(): number {
     let supply = [];
     let dRow = [];
     let dCol = [];
-
+    let c = 25;
     costsRow = cost_table.costs.slice();
     costsCol = cost_col.slice();
     amountOrder = amount_order.slice();
@@ -46,104 +50,78 @@ export function getResultado(): number {
     supply = inventory.slice();
     dRow = calcDifference(costsRow);
     dCol = calcDifference(costsCol);
-    if (Math.max(...dCol) > Math.max(...dRow)) {
-        for (let i = 0; i < costsCol.length; i++) {
-            if (dCol[i] === Math.max(...dCol)) {
-                for (let j = 0; j < costsCol[i].length; j++) {
-                    console.log(
-                        `Valor ${costsCol[i][j]} minimo ${Math.min(
-                            ...costsCol[i]
-                        )}`
-                    );
-                    if (costsCol[i][j] === Math.min(...costsCol[i])) {
-                        if (amountOrder[j] < supply[i]) {
-                            supply[i] = supply[i] - amountOrder[j];
-                            finalTable[j][i] = amountOrder[j];
-                            valueFinal =
-                                valueFinal + finalTable[j][i] * costsCol[i][j];
-                            amountOrder[j] = 0;
-
-                            if (costsCol[j] !== undefined) {
-                                for (let n = 0; n < costsRow[j].length; n++) {
+    while (c !== 0) {
+        if (Math.max(...dCol) === -1 || Math.max(...dRow) === -1) {
+            break;
+        }
+        if (Math.max(...dCol) > Math.max(...dRow)) {
+            for (let i = 0; i < costsCol.length; i++) {
+                if (dCol[i] === Math.max(...dCol)) {
+                    for (let j = 0; j < costsCol[i].length; j++) {
+                        if (costsCol[i][j] === Math.min(...costsCol[i])) {
+                            if (amountOrder[j] < supply[i]) {
+                                supply[i] = supply[i] - amountOrder[j];
+                                finalTable[j][i] = amountOrder[j];
+                                valueFinal =
+                                    valueFinal +
+                                    finalTable[j][i] * costsCol[i][j];
+                                amountOrder[j] = 0;
+                                for (let n = 0; n < costsRow[i].length; n++) {
+                                    costsCol[n][j] = 99;
+                                    costsRow[j][n] = 99;
+                                }
+                            } else {
+                                supply[i] = amountOrder[j] - supply[i];
+                                finalTable[j][i] = supply[i];
+                                valueFinal =
+                                    valueFinal +
+                                    finalTable[j][i] * costsCol[i][j];
+                                amountOrder[j] = amountOrder[j] - supply[i];
+                                for (let n = 0; n < costsCol[i].length; n++) {
+                                    costsCol[i][n] = 99;
+                                    costsRow[n][i] = 99;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            for (let i = 0; i < costsRow.length; i++) {
+                if (dRow[i] === Math.max(...dRow)) {
+                    for (let j = 0; j < costsRow[i].length; j++) {
+                        if (costsRow[i][j] === Math.min(...costsRow[i])) {
+                            if (amountOrder[i] < supply[j]) {
+                                supply[j] = supply[j] - amountOrder[i];
+                                finalTable[i][j] = amountOrder[i];
+                                valueFinal =
+                                    valueFinal +
+                                    finalTable[i][j] * costsRow[i][j];
+                                amountOrder[i] = 0;
+                                for (let n = 0; n < costsRow[i].length; n++) {
+                                    costsCol[n][i] = 99;
+                                    costsRow[i][n] = 99;
+                                }
+                            } else {
+                                supply[j] = amountOrder[i] - supply[j];
+                                finalTable[i][j] = supply[j];
+                                valueFinal =
+                                    valueFinal +
+                                    finalTable[i][j] * costsRow[i][j];
+                                amountOrder[i] = amountOrder[i] - supply[j];
+                                for (let n = 0; n < costsCol[j].length; n++) {
                                     costsCol[j][n] = 99;
                                     costsRow[n][j] = 99;
                                 }
                             }
-                            console.log(
-                                `Coluna ${Math.max(
-                                    ...dCol
-                                )} Encomendas ${amountOrder} Estoque ${supply}`
-                            );
-                        } else {
-                            supply[i] = amountOrder[j] - supply[i];
-                            finalTable[j][i] = supply[i];
-                            valueFinal =
-                                valueFinal + finalTable[j][i] * costsCol[i][j];
-                            amountOrder[j] = amountOrder[j] - supply[i];
-                            for (let n = 0; n < costsCol[i].length; n++) {
-                                costsCol[i][n] = 99;
-                                costsRow[n][i] = 99;
-                            }
-                            console.log(
-                                `Coluna ${Math.max(
-                                    ...dCol
-                                )} Encomendas ${amountOrder} Estoque ${supply}`
-                            );
                         }
                     }
                 }
             }
         }
-    } else {
-        for (let i = 0; i < costsRow.length; i++) {
-            if (dRow[i] === Math.max(...dRow)) {
-                for (let j = 0; j < costsRow[i].length; j++) {
-                    console.log(`Valor ${costsRow[i][j]}`);
-                    if (costsRow[i][j] === Math.min(...costsRow[i])) {
-                        if (amountOrder[i] < supply[j]) {
-                            supply[j] = supply[j] - amountOrder[i];
-                            finalTable[i][j] = amountOrder[i];
-                            valueFinal =
-                                valueFinal + finalTable[i][j] * costsRow[i][j];
-                            amountOrder[i] = 0;
-                            for (let n = 0; n < costsRow[i].length; n++) {
-                                costsCol[n][i] = 99;
-                                costsRow[i][n] = 99;
-                            }
-                            console.log(
-                                `Linha ${Math.max(
-                                    ...dRow
-                                )} Encomendas ${amountOrder} Estoque ${supply}`
-                            );
-                        } else {
-                            supply[j] = amountOrder[i] - supply[j];
-                            finalTable[i][j] = supply[j];
-                            valueFinal =
-                                valueFinal + finalTable[i][j] * costsRow[i][j];
-                            amountOrder[i] = amountOrder[i] - supply[j];
-                            for (let n = 0; n < costsCol[j].length; n++) {
-                                costsCol[j][n] = 99;
-                                costsRow[n][j] = 99;
-                            }
-                            console.log(
-                                `Linha ${Math.max(
-                                    ...dRow
-                                )} Encomendas ${amountOrder} Estoque ${supply}`
-                            );
-                        }
-                    }
-                }
-            }
-        }
-        console.log(costsRow);
-        console.log(costsCol);
+        dRow = calcDifference(costsRow);
+        dCol = calcDifference(costsCol);
+        c--;
     }
     return valueFinal;
-}
-
-export function vendoWhile(): void {
-    let b = 3;
-    while (b !== 0) {
-        b--;
-    }
 }
--- src/views/Result.tsx
+++ src/views/Result.tsx
@@ -4,13 +4,14 @@ import { getResultado } from '../functions';
 
 function Result(): JSX.Element {
     const [finalValue, setFinalValue] = useState(0);
-    useEffect(() => {
+    const handleResult = () => {
         setFinalValue(getResultado());
-    });
+    };
 
     return (
         <div className="App">
             <h1>Resultado Final: {finalValue} </h1>
+            <button onClick={handleResult}>Ver resultado</button>
         </div>
     );
 }
